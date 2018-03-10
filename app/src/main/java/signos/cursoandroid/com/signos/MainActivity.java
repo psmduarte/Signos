80b7e86436f2ec12062508b8c316f5c9a88f86c6
psmduarte <psmduarte@live.com>
2018-03-10 22:54:49 +0000

    Consulta de perfil de cada signo

--- app/src/main/java/signos/cursoandroid/com/signos/MainActivity.java
+++ app/src/main/java/signos/cursoandroid/com/signos/MainActivity.java
@@ -15,7 +15,7 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     private Button botao, btnSignos;
-    private TextView textDataN, textIdade;
+    private TextView textDataN, textIdade, signo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +25,14 @@ public class MainActivity extends AppCompatActivity {
         btnSignos = findViewById(R.id.btnSignosId);
         textDataN = findViewById(R.id.textDataN);
         textIdade = findViewById(R.id.textIdade);
+        signo = findViewById(R.id.signoId);
         botao = findViewById(R.id.btnId);
 
 
         btnSignos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,SignosActivity.class);
+                Intent intent = new Intent(MainActivity.this, SignosActivity.class);
                 startActivity(intent);
             }
         });
@@ -40,14 +41,13 @@ public class MainActivity extends AppCompatActivity {
         botao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 setData();
+
             }
         });
 
     }
-
-
-
 
 
     private void setData() {
@@ -60,15 +60,17 @@ public class MainActivity extends AppCompatActivity {
         DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                textDataN.setText(dayOfMonth+"/"+(month+1)+"/"+year);
-                calcularIdade( dayOfMonth, month, year);
+                textDataN.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                calcularIdade(dayOfMonth, month, year);
 
             }
-        },ano,mes,dia);
+        }, ano, mes, dia);
         datePickerDialog.show();
     }
+
+
     //Data de nascimento (N) / Data atual(A)
-    private void calcularIdade(int diaN, int mesN,int anoN) {
+    private void calcularIdade(int diaN, int mesN, int anoN) {
         Calendar c = Calendar.getInstance();
         int diaA = c.get(Calendar.DAY_OF_MONTH);
         int mesA = c.get(Calendar.MONTH);
@@ -76,14 +78,52 @@ public class MainActivity extends AppCompatActivity {
 
         int idade = anoA - anoN;
         if (mesN > mesA) {
-            idade=idade-1;//ou idade--;
+            idade = idade - 1;//ou idade--;
 
         } else if (mesA == mesN) {
             if (diaA < diaN) {
-                idade=idade-1;
+                idade = idade - 1;
             }
         }
         textIdade.setText("idade: " + idade);
+        if ((diaN >= 20 && mesN >= 1 - 1) && (diaN <= 18 && mesN <= 2 - 1)) {
+            signo.setText("Aquário");
+        }
+        if ((diaN >= 19 && mesN >= 2 - 1) && (diaN <= 20 && mesN <= 3 - 1)) {
+            signo.setText("Peixes");
+        }
+        if ((diaN >= 21 && mesN >= 3 - 1) && (diaN <= 19 && mesN <= 4 - 1)) {
+            signo.setText("Carneiro");
+        }
+        if ((diaN >= 20 && mesN >= 4 - 1) && (diaN <= 20 && mesN <= 5 - 1)) {
+            signo.setText("Touro");
+        }
+        if ((diaN >= 21 && mesN >= 5 - 1) && (diaN <= 20 && mesN <= 6 - 1)) {
+            signo.setText("Gémeos");
+        }
+        if ((diaN >= 21 && mesN >= 6 - 1) && (diaN <= 22 && mesN <= 7 - 1)) {
+            signo.setText("Aquário");
+        }
+        if ((diaN >= 20 && mesN >= 1 - 1) && (diaN <= 18 && mesN <= 2 - 1)) {
+            signo.setText("Carangueijo");
+        }
+        if ((diaN >= 23 && mesN >= 7 - 1) && (diaN <= 22 && mesN <= 8 - 1)) {
+            signo.setText("Leão");
+        }
+        if ((diaN >= 23 && mesN >= 8 - 1) && (diaN <= 22 && mesN <= 9 - 1)) {
+            signo.setText("Virgem");
+        }
+        if ((diaN >= 23 && mesN >= 9 - 1) && (diaN <= 22 && mesN <= 10 - 1)) {
+            signo.setText("Balança");
+        }
+        if ((diaN >= 23 && mesN >= 10 - 1) && (diaN <= 21 && mesN <= 11 - 1)) {
+            signo.setText("Escorpião");
+        }
+        if ((diaN >= 22 && mesN >= 11 - 1) && (diaN <= 21 && mesN <= 12 - 1)) {
+            signo.setText("Sagitário");
+        } else {
+            signo.setText("Capricórnio");
+        }
 
     }
 }
---
+++ app/src/main/java/signos/cursoandroid/com/signos/PerfilSigno.java
@@ -0,0 +1,35 @@
+package signos.cursoandroid.com.signos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PerfilSigno extends AppCompatActivity {
+
+    private TextView texto;
+
+    private String[] perfis={"O Diabo de Desafio Enérgico. Aventureiro e espontâneo. Confiante e entusiástico. Divertido. Ama um desafio. EXTREMAMENTE impaciente. Às vezes egoísta. Fusível curto (enfurece facilmente) Vivido, inteligência apaixonada e afiada. Gosta de sair. Perde interesse depressa - facilmente entediado. Egoístico. Corajoso e afirmativo. Tende a ser físico e atlético.",
+            "O Resistente. Que encanta mas agressivo. Podem parecer enfadonhos, mas não são. Trabalhadores duros. Amável. Forte, tem resistência. Seres sólidos e estáveis e seguros dos modos deles/delas. Não procuram atalhos. Orgulhosos da beleza deles/delas. Pacientes e seguros. Fazem grandes amigos e dão bons conselhos. Bom coração. Amam profundamente - apaixonados. Expressam-se emocionalmente. Propenso a temperamento-acessos de raiva ferozes. Determinado. Cedem aos seus desejos freqüentemente. Muito generoso.",
+            "O Tagarela. Inteligente e engenhoso. Parece estar sempre de saída, muito falador. Vivo, enérgico. Adaptável mas com necessidade de se expressar. Argumentativo e franco. Gosta de mudança. Versátil. Ocupado, mas às vezes nervoso e tenso. Fofoqueiros. Pode parecer superficial ou incoerente. Mas só é sujeito a mudança. Bonito fisicamente e mentalmente. 5 anos de azar se você não remeter",
+            "O Protetor Mal-humorado. Emocional. Pode ser tímido. Muito amoroso e gentil. Bonito . Sócios excelentes para vida. Projetor. Inventivo e imaginativo. Cauteloso. Tipo de pessoa Sensível. Necessidade de ser amado pelos outros. Magoa-se facilmente, mas simpático",
+            "O Chefe. Muito organizado. Precisa de ordem nas vidas deles/delas - como estar em controle. Gosta de limites. Tende a assumir tudo. Mandão. Gosta de ajudar os outros. Social e gosta de sair. Extrovertido. Generoso, amável. Sensível. Energia criativa. Confiantes neles próprios. Bons amantes. Fazer a coisa certa é importante para Leão. Atraente.",
+            "O Perfeccionista. Dominante em relações. Conservador. Sempre quer a última palavra. Argumentativo. Preocupado. Muito inteligente. Antipatiza com barulho e caos. Ansioso. Trabalhador. Leal. Bonito. Fácil falar. Difícil de agradar. Severo. Prático e muito exigente. Freqüentemente tímido. Pessimista.",
+            "O Harmonizador. Agradável a todo o mundo que se encontra com eles. Indeciso. Tem uma aeração própria sem igual. Criativo, enérgico e muito social. Odeia estar só. Calmo, generoso. Muito amoroso e bonito. Gosta de flertar. Cede muito facilmente. Tende a deixar para depois. Muito crédulo.",
+            "O Intenso. Muito enérgico. Inteligente. Pode ser ciumento e/ou possessivo. Trabalhador. Grande beijador. Pode ficar obsessivo ou reservado. Guarda rancor. Atraente. Determinado. Amores que estão em relações longas. Falador. Romântico. Pode ser às vezes egocêntrico. Apaixonado e emocional.",
+            "O Otimista Agradável Irrefletido. Não quer crescer (Peter Pan Síndroma). Favorece ego. Orgulhoso. Gosta de luxos e jogar. Social e gosta de sair. Não gosta de responsabilidades. Freqüentemente fantasia. Impaciente. Divertido estar ao seu redor. Tem muitos amigos. Coquete e gosta de flerta. Não gosta de regras. Às vezes hipócrita. Antipatiza com espaços limitados - apertados ou até mesmo roupas apertadas. Não gosta que duvidem dele. Bonito por dentro e por fora.",
+            "O Paciente (não no meu caso). Pessoa agressiva e sábio. Prático e rígido. Ambicioso. Tende a estar sempre bonito. Humorístico e engraçado. Pode ser um pouco tímido e reservado. Freqüentemente pessimistas. Capricórnio tendem a agir antes de pensar e podem ser às vezes pouco amigáveis. Guarda rancor. Gosta de competição. Obtém o que eles querem.","O Amado. Otimista e honesto. Doce personalidade. Muito independente. Inventivo e inteligente. Amigável e leal. Pode parecer não emotivo. Pode ser um pouco rebelde. Muito teimoso, mas original e sem igual. Atraente no lado de dentro e fora. Personalidade excêntrica.","O Sonhador Generoso. Bom coração e pensativo. Muito criativo e imaginativo. Pode ficar reservado e vago. Sensível. Não gosta de detalhes. Sonhador e irreal. Simpático e amoroso. Tipo. Desinteressado. Bom beijador. Bonito."};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_signo);
+
+        texto = findViewById(R.id.textoPerfilId);
+
+        Bundle extra = getIntent().getExtras();
+        int id = extra.getInt("POSITION");
+
+        texto.setText(perfis[id]);
+
+    }
+}
